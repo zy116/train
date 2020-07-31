164e5dfe315f62f06fe5f7320f0fe3612c85727a
1296424171@qq.com <1296424171@qq.com>
2020-07-31 14:28:08 +0800

    增加跨域配置

---
+++ RBAC/vue01/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Permission from "../views/Permission";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    /*方式一，使用箭头函数import*/
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/permission",
+    name: "Permission",
+    component: Permission
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
